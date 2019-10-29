78e9e2e9511a58859e17f0da85e393417e1509ce
Daniel Lohse <info@asapdesign.de>
2019-10-29 13:02:52 +0100

    Improve kind-based dev setup
    Data is now kept and not deleted when you Ctrl+C out of
    `make kind-apply`.

--- k8s/dev/entrypoint.sh
+++ k8s/dev/entrypoint.sh
@@ -2,9 +2,9 @@
 
 if [ "$HOSTNAME" = "liftbridge-0" ]
 then
-  echo "Running in liftbridge-0 as a bootstrap seed...\n"
-  liftbridge --data-dir=/data --config /etc/liftbridge.conf --level=debug --nats-servers=nats://nats.liftbridge.svc:4222 --raft-bootstrap-seed --id="$HOSTNAME"
+  echo "Running in liftbridge-0 as a bootstrap seed..."
+  liftbridge --data-dir=/data --config /etc/liftbridge.conf --nats-servers=nats://nats.liftbridge.svc:4222 --raft-bootstrap-seed --id="$HOSTNAME"
 else
-  echo "Running in $HOSTNAME...\n"
-  liftbridge --data-dir=/data --config /etc/liftbridge.conf --level=debug --nats-servers=nats://nats.liftbridge.svc:4222 --id="$HOSTNAME"
+  echo "Running in $HOSTNAME..."
+  liftbridge --data-dir=/data --config /etc/liftbridge.conf --nats-servers=nats://nats.liftbridge.svc:4222 --id="$HOSTNAME"
 fi
