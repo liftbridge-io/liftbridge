4308636b7f20f543e43c4b586ca4ea81a804b2e3
Daniel Lohse <info@asapdesign.de>
2019-11-04 07:33:44 +0100

    Add docstrings to public methods in the health package

--- server/health/health.go
+++ server/health/health.go
@@ -10,14 +10,17 @@ const serviceName = "proto.API" // taken from compiled protobuf file api.go.pb (
 
 var server = health.NewServer()
 
+// Register the health service with a gRPC server.
 func Register(srv *grpc.Server) {
 	grpc_health_v1.RegisterHealthServer(srv, server)
 }
 
+// SetServing marks the service as healthy.
 func SetServing() {
 	server.SetServingStatus(serviceName, grpc_health_v1.HealthCheckResponse_SERVING)
 }
 
+// SetNotServing marks the service as unhealthy.
 func SetNotServing() {
 	server.SetServingStatus(serviceName, grpc_health_v1.HealthCheckResponse_NOT_SERVING)
 }
