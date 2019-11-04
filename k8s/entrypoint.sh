61fb8d85b897d5bd817f00272f54d02afd9d52a2
Daniel Lohse <info@asapdesign.de>
2019-11-04 15:09:43 +0100

    Allow providing a custom cluster node name prefix via `CLUSTER_NAME` env variable

--- k8s/entrypoint.sh
+++ k8s/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 if [[ "$HOSTNAME" =~ -0$ ]];
+CLUSTER_NAME=${CLUSTER_NAME:=cluster}
 then
   echo "Running in liftbridge-0 as a bootstrap seed..."
-  liftbridge --data-dir=/data --config /etc/liftbridge.conf --raft-bootstrap-seed --id="$HOSTNAME"
+  liftbridge --data-dir=/data --config /etc/liftbridge.conf --raft-bootstrap-seed --id="${CLUSTER_NAME}-$HOSTNAME"
 else
   echo "Running in $HOSTNAME..."
-  liftbridge --data-dir=/data --config /etc/liftbridge.conf --id="$HOSTNAME"
+  liftbridge --data-dir=/data --config /etc/liftbridge.conf --id="${CLUSTER_NAME}-$HOSTNAME"
 fi
