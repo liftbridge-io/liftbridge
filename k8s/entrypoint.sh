ffcc737f4b15476c23258419f085c29531c34f03
Tyler Treat <ttreat31@gmail.com>
2020-04-27 16:21:31 -0600

    Fix skaffold configuration

--- k8s/entrypoint.sh
+++ k8s/entrypoint.sh
@@ -5,8 +5,8 @@ CLUSTER_NAME=${CLUSTER_NAME:=cluster}
 if [[ "$HOSTNAME" =~ -0$ ]] && [[ "$SKIP_SEED" != "true" ]];
 then
   echo "Running in liftbridge-0 as a bootstrap seed..."
-  liftbridge --data-dir=/data --config /etc/liftbridge.conf --raft-bootstrap-seed --id="${CLUSTER_NAME}-$HOSTNAME"
+  liftbridge --data-dir=/data --config /etc/liftbridge.yaml --raft-bootstrap-seed --id="${CLUSTER_NAME}-$HOSTNAME"
 else
   echo "Running in $HOSTNAME..."
-  liftbridge --data-dir=/data --config /etc/liftbridge.conf --id="${CLUSTER_NAME}-$HOSTNAME"
+  liftbridge --data-dir=/data --config /etc/liftbridge.yaml --id="${CLUSTER_NAME}-$HOSTNAME"
 fi
