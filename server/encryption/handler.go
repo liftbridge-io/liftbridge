144a2af40a96ce4722e05d8cc22add12165993eb
Tung Hoang <sontunghoang@gmail.com>
2021-02-28 17:14:53 +0100

    Add decrypt function

--- server/encryption/handler.go
+++ server/encryption/handler.go
@@ -15,7 +15,7 @@ const (
 	// It is recommended to use an authentication key with 32 or 64 bytes.
 	// The encryption key, if set, must be either
 	// 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256 modes.
-	EncryptionKeyLength int = 24
+	EncryptionKeyLength int = 32
 )
 
 var (
@@ -34,19 +34,13 @@ type Handler interface {
 // LocalEncryptionHandler provides functionalities to load secret key
 // from environment variables
 type LocalEncryptionHandler struct {
-	defaultDKS   []byte
-	keywrappeder *subtle.KWP
-	blockCypher  *cipher.AEAD
+	keywrapper  *subtle.KWP
+	blockCypher *cipher.AEAD
 }
 
 // NewLocalEncriptionHandler generates a new instance of LocalEncryptionHandler
 func NewLocalEncriptionHandler() (*LocalEncryptionHandler, error) {
 	localEncryptionHandler := LocalEncryptionHandler{}
-
-	// generate default DKS
-
-	defaultDKS, err := localEncryptionHandler.generateDKS()
-	localEncryptionHandler.defaultDKS = defaultDKS
 
 	// Init key wrapper
 
@@ -57,21 +51,7 @@ func NewLocalEncriptionHandler() (*LocalEncryptionHandler, error) {
 	if err != nil {
 		return nil, err
 	}
-	localEncryptionHandler.keywrappeder = kwp
-
-	// Init cypher in GCM
-
-	block, err := aes.NewCipher(localEncryptionHandler.defaultDKS)
-	if err != nil {
-		return nil, err
-	}
-
-	gcm, err := cipher.NewGCM(block)
-
-	if err != nil {
-		return nil, err
-	}
-	localEncryptionHandler.blockCypher = &gcm
+	localEncryptionHandler.keywrapper = kwp
 
 	return &localEncryptionHandler, nil
 }
@@ -94,7 +74,7 @@ func (handler *LocalEncryptionHandler) generateDKS() ([]byte, error) {
 func (handler *LocalEncryptionHandler) wrapDKS(dks []byte) ([]byte, error) {
 	// use Tinker to wrapped Data Key
 	// https://github.com/google/tink/commit/22467ef7273d73b2d65e4b50310aab4af006bb7e
-	wrappededKey, err := handler.keywrappeder.Wrap(dks)
+	wrappededKey, err := handler.keywrapper.Wrap(dks)
 
 	if err != nil {
 		return nil, err
@@ -105,12 +85,50 @@ func (handler *LocalEncryptionHandler) wrapDKS(dks []byte) ([]byte, error) {
 }
 
 func (handler *LocalEncryptionHandler) encryptData(dks []byte, plaintextData []byte) ([]byte, error) {
-	gcm := *handler.blockCypher
+	// Init cypher in GCM
+
+	block, err := aes.NewCipher(dks)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+
+	if err != nil {
+		return nil, err
+	}
 	// init nonce
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
 		panic(err.Error())
 	}
-	ciphertext := gcm.Seal(nil, nonce, plaintextData, nil)
+	ciphertext := gcm.Seal(nonce, nonce, plaintextData, nil)
 	return ciphertext, nil
+}
+
+func (handler *LocalEncryptionHandler) decryptData(dks []byte, encryptedData []byte) ([]byte, error) {
+
+	block, err := aes.NewCipher(dks)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// get nonce
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := encryptedData[:nonceSize], encryptedData[nonceSize:]
+
+	//Decrypt the data
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
 }
--- server/encryption/handler_test.go
+++ server/encryption/handler_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// Ensure that a data key can be generated
 func TestGenerateDataKeyCorrectly(t *testing.T) {
 	// Set a random AES key as master key
 	os.Setenv("LOCAL_MASTER_KEY", "+KbPeShVmYq3t6w9")
@@ -23,6 +24,7 @@ func TestGenerateDataKeyCorrectly(t *testing.T) {
 	require.NotEmpty(t, dks)
 }
 
+// ENsure that key can be wrapped by using master key
 func TestWrapDataKeyCorrectly(t *testing.T) {
 	// Set a random AES key as master key
 	os.Setenv("LOCAL_MASTER_KEY", "t7w!z%C*F-JaNcRf")
@@ -49,6 +51,7 @@ func TestWrapDataKeyCorrectly(t *testing.T) {
 	require.NotEqual(t, dks, wrappedDKS)
 }
 
+// Ensure encryptions can encrypt the message
 func TestEncryption(t *testing.T) {
 	// Set a random AES key as master key
 	os.Setenv("LOCAL_MASTER_KEY", "t7w!z%C*F-JaNcRf")
@@ -60,7 +63,12 @@ func TestEncryption(t *testing.T) {
 	// Given sample data
 	plaintext := []byte("exampleplaintext")
 
-	ciphertext, err := keyHandler.encryptData(keyHandler.defaultDKS, plaintext)
+	// Given a sample data key
+
+	dks, err := keyHandler.generateDKS()
+	require.NoError(t, err)
+
+	ciphertext, err := keyHandler.encryptData(dks, plaintext)
 	require.NoError(t, err)
 
 	require.NotEmpty(t, ciphertext)
@@ -68,4 +76,32 @@ func TestEncryption(t *testing.T) {
 	// Expect cipher  text should not be the same
 	// as plain text
 	require.NotEqual(t, ciphertext, plaintext)
+}
+
+// Ensure that the decryption retrieves the same text after encryption
+func TestDecryption(t *testing.T) {
+	// Set a random AES key as master key
+	os.Setenv("LOCAL_MASTER_KEY", "t7w!z%C*F-JaNcRf")
+
+	// Given a key handler
+	keyHandler, err := NewLocalEncriptionHandler()
+	require.NoError(t, err)
+
+	// Given sample data
+	plaintext := []byte("exampleplaintext")
+
+	// Given a sample data key
+	dks, err := keyHandler.generateDKS()
+	require.NoError(t, err)
+
+	// Encrpyt
+	ciphertext, err := keyHandler.encryptData(dks, plaintext)
+	require.NoError(t, err)
+
+	// Decrypt
+	decryptedText, err := keyHandler.decryptData(dks, ciphertext)
+	require.NoError(t, err)
+
+	// Expect to retrieve the same original text
+	require.Equal(t, plaintext, decryptedText)
 }
